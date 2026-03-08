efbb2aed84c45d6500d35f81ae1087c5c56c823c
zhou108-z <zhouyuan544@outlook.com>
2026-03-08 19:03:39 +0800

    Add files via upload

---
+++ lab01/src/main/java/com/example/lab01/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.lab01;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ lab01/src/main/java/com/example/lab01/helloservlet.java
@@ -0,0 +1,32 @@
+package com.example.lab01;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// 仅把 javax 换成 jakarta，其余代码和PPT完全一致
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloServlet", value = "/hello-servlet")
+public class helloservlet {
+
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
